0b1b3facbb9c93a6415e3a569434a303c039b9e6
Khaledxab <khaledxab@gmail.com>
2022-09-21 00:27:43 +0200

    async routine called wait_n

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,14 @@
+# /usr/bin/env python3
+
+import asyncio
+from typing import List
+import random
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """ return the list of all the delays """
+    delays = [await wait_random(max_delay) for i in range(n)]
+    return sorted(delays)
