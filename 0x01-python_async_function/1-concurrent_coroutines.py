c669276f2983d0cae6f1085e14c539b8d42efd74
Khaledxab <khaledxab@gmail.com>
2022-09-21 00:41:00 +0200

    async routine called wait_n

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -12,5 +12,5 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """ return the list of all the delays """
-    delays = [await wait_random(max_delay) for i in range(n)]
-    return sorted(delays)
+    delays = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    return [await item for item in asyncio.as_completed(delays)]
