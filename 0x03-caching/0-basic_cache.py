98dc62a40e908c5064961ebff25b44648c6c919c
Khaledxab <khaledxab@gmail.com>
2022-09-23 20:49:24 +0200

    BasicCache

---
+++ 0x03-caching/0-basic_cache.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" """
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ BasicCache class """
+
+    def put(self, key, item):
+        """ PUT """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ GET """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
