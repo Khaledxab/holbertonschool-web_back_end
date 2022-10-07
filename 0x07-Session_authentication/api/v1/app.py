25ef92fcd0ee9c9ec70d972a5d9bcef727a49266
Khaledxab <khaledxab@gmail.com>
2022-10-07 17:06:00 +0200

    Before Request

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -54,7 +54,8 @@ def before_request() -> str:
                       '/api/v1/forbidden/']
     if not auth.require_auth(request.path, excluded_paths):
         return
-    if auth.authorization_header(request) is None:
+    if not auth.authorization_header(request)\
+       and not auth.session_cookie(request): 
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
