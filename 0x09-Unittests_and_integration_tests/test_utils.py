02dfaa5af1abc8580ffcdabc37f3cc4a909add48
Khaledxab <khaledxab@gmail.com>
2022-10-18 17:04:55 +0200

    add map exception

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -6,7 +6,7 @@ from utils import access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """ test for access_nested_map """
+    """ to test the function for following inputs """
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
@@ -16,12 +16,13 @@ class TestAccessNestedMap(unittest.TestCase):
         """ method to test that the method returns what it is supposed to """
         self.assertEqual(access_nested_map(nested_map, path), answer)
 
-    """ test that a KeyError is raised for the following inputs """
+    """  to test that a KeyError is raised for the following inputs """
     @parameterized.expand([
+        ({}, ("a",)),
         ({"a": 1}, ("a", "b")),
     ])
     def test_access_nested_map_exception(self, nested_map, path):
-        """ method to test that the method raises
-        an exception when it is supposed to """
-        with self.assertRaises(KeyError):
+        """ method to test that a KeyError is raised properly """
+        with self.assertRaises(KeyError) as error:
             access_nested_map(nested_map, path)
+        self.assertEqual(error.exception.args[0], path[-1])
