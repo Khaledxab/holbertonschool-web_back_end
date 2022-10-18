2bb5d248804942120c7d1b520e90b8fb4a883d57
Khaledxab <khaledxab@gmail.com>
2022-10-18 19:27:38 +0200

    Parameterize and patch

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-""" Parameterize a unit test, Mock HTTP calls """
+""" Parameterize a unit test, Mock HTTP calls. PATCH """
 import unittest
 from unittest.mock import patch
 from parameterized import parameterized
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -42,3 +42,25 @@ class TestGetJson(unittest.TestCase):
         mock_get.return_value = test_payload
         result = get_json(test_url)
         self.assertEqual(result, test_payload)
+
+class TestMemoize(unittest.TestCase):
+    """ to test the function for following inputs """
+    def test_memoize(self):
+        """ test that the method returns what it is supposed to """
+        class TestClass:
+            """ test class """
+            def a_method(self):
+                """ test method """
+                return 42
+
+            @memoize
+            def a_property(self):
+                """ test property """
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method') as mock_method:
+            mock_method.return_value = 42
+            test = TestClass()
+            test.a_property
+            test.a_property
+            mock_method.assert_called_once()
