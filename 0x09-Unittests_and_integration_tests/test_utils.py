13ef8a40da1f05336ff70aafeaa91fad9cec1de5
Khaledxab <khaledxab@gmail.com>
2022-10-18 16:49:21 +0200

    add map exception

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -7,6 +7,7 @@ from utils import access_nested_map
 
 class TestAccessNestedMap(unittest.TestCase):
     """ testcase """
+    """ test for access_nested_map """
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
