42c58e7e6c7c70e4406196111f082f806e7308ad
Khaledxab <khaledxab@gmail.com>
2022-10-19 18:52:53 +0200

    Integration test: fixtures

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -3,8 +3,10 @@
 and more patching """
 import unittest
 from unittest.mock import patch, PropertyMock
-from parameterized import parameterized
+from parameterized import parameterized, parameterized_class
 from client import GithubOrgClient
+from fixtures import TEST_PAYLOAD
+from urllib.error import HTTPError
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -61,3 +63,19 @@ class TestGithubOrgClient(unittest.TestCase):
         test_client = GithubOrgClient("khaledxab")
         result = test_client.has_license(repo, license_key)
         self.assertEqual(result, expected)
+
+@parameterized_class(
+    ("org_payload", "repos_payload", "expected_repos", "apache2_repos"),
+    TEST_PAYLOAD
+)
+class TestIntegrationGithubOrgClient(unittest.TestCase):
+    """ TESTCASE """
+    @classmethod
+    def setUpClass(cls):
+        """ APImethod to return example payload """
+        cls.get_patcher = patch('requests.get', side_effect=HTTPError)
+
+    @classmethod
+    def tearDownClass(cls):
+        """ API method to stop the patcher"""
+        cls.get_patcher.stop()
