fdfdbfee24e2b53e39f16e5a70b91910cebe101b
Khaledxab <khaledxab@gmail.com>
2022-10-19 17:17:49 +0200

    change method

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -30,17 +30,17 @@ class TestGithubOrgClient(unittest.TestCase):
         """ test that the method returns correct value """
         test_payload = {"repos_url": "holberton"}
         with patch.object(GithubOrgClient,
-                            "org",
-                            new_callable=PropertyMock) as mock_org:
-                mock_org.return_value = test_payload
-                result = GithubOrgClient("holberton")
-                self.assertEqual(result._public_repos_url, "holberton")
+                          "org",
+                          new_callable=PropertyMock) as mock_org:
+            mock_org.return_value = test_payload
+            result = GithubOrgClient("holberton")
+            self.assertEqual(result._public_repos_url, "holberton")
 
     @patch("client.get_json", return_value=[{"name": "holberton"}])
     def test_public_repos(self, mock_get):
         """ test that the method returns correct value """
         with patch.object(GithubOrgClient, '_public_repos_url',
-                            new_callable=PropertyMock) as mock_url:
+                          new_callable=PropertyMock) as mock_url:
             mock_url.return_value = "holberton"
             result = GithubOrgClient("holberton")
             self.assertEqual(result.public_repos(), ["holberton"])
