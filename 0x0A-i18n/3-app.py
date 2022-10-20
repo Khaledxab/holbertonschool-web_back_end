49dd97e3ef809cbfbc85347ffbda4c2d420f7a16
Khaledxab <khaledxab@gmail.com>
2022-10-20 21:06:27 +0200

    Parametrize templates translation

--- 0x0A-i18n/3-app.py
+++ 0x0A-i18n/3-app.py
@@ -2,22 +2,20 @@
 """ Basic Flask app, Basic Babel setup, Get locale from request,
     Parametrize templates """
 from flask import Flask, render_template, request
-from flask_babel import Babel, gettext
-
+from flask_babel import Babel
 app = Flask(__name__)
+
 babel = Babel(app)
-""" instantiate the Babel object """
+
 
 class Config(object):
     """ Config class """
     LANGUAGES = ["en", "fr"]
-
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app.config.from_object(Config)
-"""use config class for flask"""
 
 
 @babel.localeselector
