610048d94c47fe55de1551f7ef99e437031e48c7
Khaledxab <khaledxab@gmail.com>
2022-10-20 22:25:42 +0200

    Mock logging in

--- 0x0A-i18n/5-app.py
+++ 0x0A-i18n/5-app.py
@@ -6,13 +6,14 @@ from flask_babel import Babel
 app = Flask(__name__)
 
 babel = Babel(app)
-
+""" instantiate the Babel object """
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
     2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
     3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
     4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
 }
+""" mock database of users """
 
 
 class Config(object):
@@ -25,20 +26,28 @@ class Config(object):
 app.config.from_object(Config)
 
 
+@app.route('/')
+def root():
+    """ Root route """
+    return render_template('5-index.html')
+
+
 @babel.localeselector
 def get_locale():
-
+    """ Get locale from request """
     locale = request.args.get('locale')
     if locale and locale in app.config['LANGUAGES']:
         return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-def get_user(login_as):
-    """ Get user from users """
-    if login_as and login_as in users:
-        return users.get(int(login_as))
-    return None
+def get_user():
+    """ Get user from database """
+    try:
+        user_id = int(request.args.get('login_as'))
+    except Exception:
+        return None
+    return users.get(user_id)
 
 
 @app.before_request
@@ -47,12 +56,6 @@ def before_request():
     user = get_user(request.args.get('login_as'))
     if user:
         g.user = user
-
-
-@app.route('/')
-def root():
-    """ Root route """
-    return render_template('5-index.html')
 
 
 if __name__ == "__main__":
