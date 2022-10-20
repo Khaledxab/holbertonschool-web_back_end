5dbdbb72458fcdd7cc6a778632e61503f18d803b
Khaledxab <khaledxab@gmail.com>
2022-10-20 22:22:05 +0200

    Mock logging in

--- 0x0A-i18n/5-app.py
+++ 0x0A-i18n/5-app.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
 """ Basic Flask app, Basic Babel setup, Get locale from request,
     Parametrize templates, Force locale with URL parameter """
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g
 from flask_babel import Babel
 app = Flask(__name__)
 
 babel = Babel(app)
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
 
 
 class Config(object):
@@ -25,6 +32,21 @@ def get_locale():
     if locale and locale in app.config['LANGUAGES']:
         return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+def get_user(login_as):
+    """ Get user from users """
+    if login_as and login_as in users:
+        return users.get(int(login_as))
+    return None
+
+
+@app.before_request
+def before_request():
+    """ Before request """
+    user = get_user(request.args.get('login_as'))
+    if user:
+        g.user = user
 
 
 @app.route('/')
