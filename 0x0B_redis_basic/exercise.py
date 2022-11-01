9f360f06f80856b5a8b7f9cf4546050d65f5710d
Khaledxab <khaledxab@gmail.com>
2022-11-01 16:39:27 +0100

    Writing strings to Redis

---
+++ 0x0B_redis_basic/exercise.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Writing strings to Redis"""
+import redis
+import uuid
+from typing import Union, Callable
+
+
+class Cache:
+    """Cache class"""
+    def __init__(self):
+        """Constructor"""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """Store data in Redis"""
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
+
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float]:
+        """Get data from Redis"""
+        data = self._redis.get(key)
+        if fn:
+            return fn(data)
+        return data
