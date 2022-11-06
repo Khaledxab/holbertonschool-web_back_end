58f85234bff6a06f4109e6dbaa6e865e71db6cc9
Khaledxab <khaledxab@gmail.com>
2022-11-06 22:16:27 +0100

    Old school band

--- 0x0C-MySQL_Advanced/3-glam_rock.sql
+++ 0x0C-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,5 @@
--- SQL script that lists all bands with Glam rock as their main style,
--- ranked by their longevity
+-- SQL script that lists all bands with Glam rock as their main style
 SELECT band_name, COALESCE(split, 2022) - formed AS lifespan
 FROM metal_bands
-WHERE style = 'Glam rock'
+WHERE style LIKE 'Glam rock'
 ORDER BY lifespan DESC;
