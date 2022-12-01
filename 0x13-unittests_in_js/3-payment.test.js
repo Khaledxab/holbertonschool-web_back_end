c1d304df62ae27b0ac362d41bd9dd1440ef99790
khaledxab <khaledxab@gmail.com>
2022-12-01 20:57:13 +0100

    Spies

---
+++ 0x13-unittests_in_js/3-payment.test.js
@@ -0,0 +1,15 @@
+'use strict';
+const chai = require('chai');
+const sinon = require('sinon');
+
+const Utils = require('./utils.js');
+const sendPaymentRequestToApi = require('./3-payment.js');
+
+describe('sendPaymentRequestToApi function', () => {
+    it('validate the usage of the Utils function', () => {
+        const spy = sinon.spy(Utils, 'calculateNumber');
+        sendPaymentRequestToApi(100, 20);
+        chai.expect(spy.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+        spy.restore();
+    });
+    });
