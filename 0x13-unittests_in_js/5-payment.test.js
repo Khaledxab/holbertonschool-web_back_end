e59f6b4c989688447edeb02fa52a920108a383e8
khaledxab <khaledxab@gmail.com>
2022-12-01 23:12:42 +0100

    Hooks

--- 0x13-unittests_in_js/5-payment.test.js
+++ 0x13-unittests_in_js/5-payment.test.js
@@ -6,15 +6,18 @@ const Utils = require('./utils.js');
 const sendPaymentRequestToApi = require('./5-payment.js');
 
 describe('sendPaymentRequestToApi function', () => {
-    const spyConsole = sinon.spy(console, 'log');
-    it('validate the usage of the Utils function', () => {
-      const stubUtils = sinon.stub(Utils, 'calculateNumber');
-      stubUtils.withArgs('SUM', 100, 20).returns(10);
-      sendPaymentRequestToApi(100, 20);
-      chai.expect(spyConsole.calledOnce).to.be.true;
-      chai.expect(spyConsole.calledWith('The total is: 10')).to.be.true;
-      stubUtils.restore()
-      spyConsole.restore();
-    });
-  }
-);
+  let spyConsole;
+  beforeEach(() => spyConsole = sinon.spy(console, 'log'));
+  afterEach(() => spyConsole.restore());
+
+  it('sendPaymentRequestToAPI(100, 20)', () => {
+    sendPaymentRequestToApi(100, 20);
+    chai.expect(spyConsole.calledOnce).to.be.true;
+    chai.expect(spyConsole.calledWith('The total is: 120')).to.be.true;
+  });
+  it('sendPaymentRequestToAPI(10, 10)', () => {
+    sendPaymentRequestToApi(10, 10);
+    chai.expect(spyConsole.calledOnce).to.be.true;
+    chai.expect(spyConsole.calledWith('The total is: 20')).to.be.true;
+  });
+});
