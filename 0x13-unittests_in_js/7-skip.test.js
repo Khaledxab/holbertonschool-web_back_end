d9e9fc56f69ff6361b09a0073bd4194119196f75
khaledxab <khaledxab@gmail.com>
2022-12-05 11:14:55 +0100

    skip test ✔

---
+++ 0x13-unittests_in_js/7-skip.test.js
@@ -0,0 +1,11 @@
+const { expect } = require('chai');
+
+describe('Testing numbers', () => {
+    it('should return true if the number is even', () => {
+        expect(2 + 2).to.equal(4);
+    });
+    it('should return false if the number is odd', () => {
+        expect(3 + 3).to.equal(6);
+    });
+    });
+
