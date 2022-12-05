c05ca253d73931bb704e95f949d382065d4491ac
khaledxab <khaledxab@gmail.com>
2022-12-05 11:52:24 +0100

    Basic Integration testing ✌

--- 0x13-unittests_in_js/8-api/api-test.js
+++ 0x13-unittests_in_js/8-api/api-test.js
@@ -3,17 +3,17 @@ const request = require('request');
 const chai = require('chai');
 
 describe('basic integration testing', () => {
-    it('Main page content', (done) => {
-        request('http://localhost:7865', (err, res, body) => {
-            chai.expect(body).to.equal('Welcome to the payment system');
-            done();
-        });
+  describe('GET /', () => {
+    it('endpoint GET /', (done) => {
+      const call = {
+        url: 'http://localhost:7865',
+        method: 'GET',
+      };
+      request(call, (error, response, body) => {
+        chai.expect(response.statusCode).to.equal(200);
+        chai.expect(body).to.equal('Welcome to the payment system');
+        done();
+      });
     });
-    it('Main page status', (done) => {
-        request('http://localhost:7865', (err, res, body) => {
-            chai.expect(res.statusCode).to.equal(200);
-            done();
-        });
-    });
-}
-);
+  });
+});
